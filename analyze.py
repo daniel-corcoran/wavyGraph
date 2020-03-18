51adb3fd3aa7b8e286d90012dfafd415050ed97f
Joe Welage <welagejl@mail.uc.edu>
2020-03-18 20:09:46 -0300

    added data collect

---
+++ analyze.py
@@ -0,0 +1,14 @@
+from scipy.io import wavfile
+import numpy as np
+fs, data = wavfile.read('Waltz-music-loop.wav')
+
+data = data.astype(np.float32)
+data= (data / np.max(np.abs(data)))
+data -= np.mean(data)
+def yield_data():
+    for i in data:
+        yield i
+print(len(data))
+absData = []
+for i in data:
+    data = np.abs(i)
